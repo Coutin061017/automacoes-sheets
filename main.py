c918d07b284deaabdb82e67a87e3a940f7e3a96e
André Coutinho <andre9heber@gmail.com>
2026-09-21 12:49:08 -0300

    refactor: reorganiza automação do Google Sheets

--- main.py
+++ main.py
@@ -1,53 +1,25 @@
 import os
-
 import gspread
 import pandas as pd
 from dotenv import load_dotenv
 
-
-# ==========================================
 #  CARREGAR CONFIGURAÇÕES
-# ==========================================
-
 load_dotenv()
-
 credenciais = os.getenv("GOOGLE_CREDENTIALS")
 planilha_id = os.getenv("GOOGLE_SHEETS_ID")
-http = os.getenv("HTTP_PROXY")
-https = os.getenv("HTTPS_PROXY")
-local = os.getenv("NO_PROXY")
-
-# Conecta utilizando a conta de serviço
-gc = gspread.service_account(
-    filename=credenciais
-)
-
-
-# Abre a planilha pelo ID
+gc = gspread.service_account(filename=credenciais)
 planilha = gc.open_by_key(planilha_id)
-
-
 print("Conexão realizada com sucesso!")
 print("Nome da planilha:", planilha.title)
 
-
 # LER A ABA ALUNOS
-
-
 aba = planilha.worksheet("Alunos")
-
 dados = aba.get_all_records()
-
 df = pd.DataFrame(dados)
-
-
 print("\nlido todos os dados da planilha:")
 print(df)
 
-# Filtra somente os alunos pendentes
 pendentes = df[df["Status"] == "Pendente"]
-
-
 print("Alunos pendentes:")
 print(pendentes)
 
@@ -61,14 +33,9 @@ except gspread.WorksheetNotFound:
         rows=100,
         cols=10
     )
-
-
-    # ENVIAR O RESULTADO PARA A ABA
-# ==========================================
-
+# ENVIAR O RESULTADO PARA A SEGUDA ABA
 valores = [
-    pendentes.columns.tolist()
-] + pendentes.values.tolist()
+    pendentes.columns.tolist()] + pendentes.values.tolist()
 
 aba_pendentes.clear()
 
@@ -76,7 +43,6 @@ aba_pendentes.update(
     "A1",
     valores
 )
-
 
 print("\n================================")
 print("Processo concluído!")
